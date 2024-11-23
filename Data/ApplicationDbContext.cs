a988b7678be86f0b54951d6500d9c86f26b93b6e
efestrikesback <96078086+efestrikesback@users.noreply.github.com>
2024-11-23 15:31:56 +0300

    #3 initial ef migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProfitPilot.Models;
+
+namespace ProfitPilot.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<User>? Users { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using ProfitPilot.Components;
+using Microsoft.EntityFrameworkCore;
+using ProfitPilot.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
